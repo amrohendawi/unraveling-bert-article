8981a1fb21e98806db45161895ca639e064be033
amrohendawi <amro.hendawi@gmail.com>
2022-07-14 14:33:55 +0200

    scrollEventHandler fixed to wait until scrolling stops

--- assets/script.js
+++ assets/script.js
@@ -1,16 +1,21 @@
 // an event listener that triggers the function getVisibleElements()
 window.addEventListener('scroll', checkScroll, false);
+var isScrolling;
 
 function checkScroll() {
-    // first wait until the scolling is finished
-    if (window.requestAnimationFrame) {
-        requestAnimationFrame(function () {
-            var elements = getVisibleElements();
-            if (elements.length > 0) {
-                document.getElementById(elements[elements.length - 1].id + '-button').click();
-            }
-        });
-    }
+window.clearTimeout( isScrolling );
+	// Set a timeout to run after scrolling ends
+	isScrolling = setTimeout(function() {
+		// Run the callback
+        if (window.requestAnimationFrame) {
+            requestAnimationFrame(function () {
+                var elements = getVisibleElements();
+                if (elements.length > 0) {
+                    document.getElementById(elements[elements.length - 1].id + '-button').click();
+                }
+            });
+        }
+	}, 66);
 }
 
 // a function that returns all the elements that are visible in the screen
@@ -28,14 +33,14 @@ function getVisibleElements() {
     return elements;
 }
 
-document.addEventListener('click', function (e) {
-    // remove the scroll event listener when a button is clicked
-    window.removeEventListener('scroll', checkScroll, false);
-    if (e.target.classList.contains('button')) {
-        e.preventDefault();
-    }
-    //    add the scroll event listener back after 1 second
-    setTimeout(function () {
-        window.addEventListener('scroll', checkScroll, false);
-    }, 1000);
-});
+// document.addEventListener('click', function (e) {
+//     // remove the scroll event listener when a button is clicked
+//     window.removeEventListener('scroll', checkScroll, false);
+//     if (e.target.classList.contains('button')) {
+//         e.preventDefault();
+//     }
+//     //    add the scroll event listener back after 1 second
+//     setTimeout(function () {
+//         window.addEventListener('scroll', checkScroll, false);
+//     }, 1000);
+// });
